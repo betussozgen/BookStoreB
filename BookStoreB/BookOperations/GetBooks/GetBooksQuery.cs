2cc5babce4c6f751a1042b4b94040151848d52a7
betussozgen <fatmabetulozgen@hotmail.com>
2023-07-30 16:28:59 +0300

    Model Kullanımı

---
+++ BookStoreB/BookOperations/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,24 @@
+﻿using BookStoreB.DBOperations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreB.BookOperations.GetBooks
+{
+    public class GetBooksQuery
+    {
+
+        private readonly BookStoreDbContext _dbContext;
+
+        //Constructor
+        public GetBooksQuery(BookStoreDbContext dbContext)
+        { 
+            _dbContext = dbContext; 
+        }
+
+        //asıl işi yapacak metotum.
+        public List<Book> Handle()
+        {
+            var bookList = _dbContext.Books.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+    }
+}
--- BookStoreB/Controllers/BookController.cs
+++ BookStoreB/Controllers/BookController.cs
@@ -15,36 +15,7 @@ namespace BookStoreB.Controllers
         {
             _context = context; 
         }
-        //private static List<Book> BookList = new List<Book>()
-        //{
-        //    new Book
-        //    {
-        //        Id = 1,
-        //        Title = "Lean Startup",
-        //        GenreId = 1,
-        //        PageCount = 200,
-        //        PublishDate = new DateTime(2001,06,12)
-        //    },
-        //    new Book
-        //    {
-        //        Id = 2,
-        //        Title = "Herland",
-        //        GenreId = 2,
-        //        PageCount = 250,
-        //        PublishDate = new DateTime(2010,05,23)
-        //    },
-        //    new Book
-        //    {
-        //        Id = 3,
-        //        Title = "Dune",
-        //        GenreId = 2,
-        //        PageCount = 540,
-        //        PublishDate = new DateTime(2001,12,21)
-        //    }
-
-        //};
-
-
+        
         [HttpGet]
         public List<Book> GetBooks()
         {
